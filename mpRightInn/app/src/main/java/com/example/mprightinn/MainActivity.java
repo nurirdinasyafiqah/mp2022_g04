b3e231f26053a66fbf518232fd43fe4101e6a274
nurirdinasyafiqah <99268943+nurirdinasyafiqah@users.noreply.github.com>
2022-03-16 01:23:19 +0800

    new code until part 7

---
+++ mpRightInn/app/src/main/java/com/example/mprightinn/MainActivity.java
@@ -0,0 +1,117 @@
+package com.example.mprightinn;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText Name;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
+    private int counter = 5;
+    private TextView userRegistration;
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog processDialog;
+    private TextView forgotPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Name = (EditText) findViewById(R.id.editTextTextPersonName);
+        Password = (EditText) findViewById(R.id.editTextTextPassword);
+        Info = (TextView) findViewById(R.id.textView);
+        Login = (Button) findViewById(R.id.button);
+        userRegistration = (TextView) findViewById(R.id.textView3);
+        forgotPassword = (TextView)findViewById(R.id.tvForgotPassword);
+
+        Info.setText("No. of attempts remaining: 5");
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        processDialog = new ProgressDialog(this);
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        if(user != null){
+            finish();
+            startActivity(new Intent(MainActivity.this, SecondActivity.class));
+        }
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
+
+        userRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RegistrationActivity.class));
+            }
+        });
+
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, PasswordActivity.class));
+            }
+        });
+    }
+
+    private void validate(String userName, String userPassword){
+
+        processDialog.setMessage("You can start your account until you are verified!");
+        processDialog.show();
+
+        firebaseAuth.signInWithEmailAndPassword(userName, userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    processDialog.dismiss();
+                    //Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                    checkEmailVerification();
+                }else{
+                    Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                    counter--;
+                    Info.setText("No. of attempts remaining: " + counter);
+                    processDialog.dismiss();
+                    if(counter == 0){
+                        Login.setEnabled(false);
+                    }
+                }
+            }
+        });
+    }
+
+    private void checkEmailVerification(){
+        FirebaseUser firebaseUser = firebaseAuth.getInstance().getCurrentUser();
+        Boolean emailflag = firebaseUser.isEmailVerified();
+
+        if(emailflag){
+            finish();
+            startActivity(new Intent(MainActivity.this, SecondActivity.class));
+        }else{
+            Toast.makeText(this, "Verify your email", Toast.LENGTH_SHORT).show();
+            firebaseAuth.signOut();
+        }
+    }
+
+}
---
+++ mpRightInn/app/src/main/java/com/example/mprightinn/UserProfile.java
@@ -0,0 +1,14 @@
+package com.example.mprightinn;
+
+public class UserProfile {
+    public String userName;
+    public String userAge;
+    public String userEmail;
+
+
+    public UserProfile(String userName, String userAge, String userEmail){
+        this.userName = userName;
+        this.userAge = userAge;
+        this.userEmail = userEmail;
+    }
+}
