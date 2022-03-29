82c6a7ab4f22b7740daecfc8396625fe064dd2a5
ariffathras <ariff.athras313@gmail.com>
2022-03-29 22:00:14 +0800

    part 11 (ada error)

--- mpRightInn/app/src/main/java/com/example/mprightinn/ProfileActivity.java
+++ mpRightInn/app/src/main/java/com/example/mprightinn/ProfileActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.icu.lang.UScript;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,12 +14,16 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
 import java.net.HttpCookie;
 
@@ -29,6 +34,7 @@ public class ProfileActivity extends AppCompatActivity {
     private Button profileUpdate, changePassword;
     private FirebaseAuth firebaseAuth;
     private FirebaseDatabase firebaseDatabase;
+    private FirebaseStorage firebaseStorage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,8 +52,18 @@ public class ProfileActivity extends AppCompatActivity {
 
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseStorage = FirebaseStorage.getInstance();
 
         DatabaseReference databaseReference = firebaseDatabase.getReference(firebaseAuth.getUid());
+
+        StorageReference storageReference = firebaseStorage.getReference();
+        storageReference.child(firebaseAuth.getUid()).child("Images/Profile Pic").getDownloadUrl().addOnCompleteListener(new OnSuccessListener<>(){
+            @Override
+            public void onSucces(Uri uri){
+                Picasso.get().load(uri).fit().centerCrop().into(profilePic);
+            }
+
+        });
 
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
